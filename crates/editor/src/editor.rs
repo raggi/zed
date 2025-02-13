48605e49c33b0efeb2aa4564d6870d23a942cd53
Michael Sloan <michael@zed.dev>
2025-02-13 00:48:34 -0700

    Rename `EditPredictionsMode::Auto` back to `WhenHoldingModifier`

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -4790,8 +4790,8 @@ impl Editor {
                     provider.provider.show_completions_in_menu()
                 });
 
-        let preview_requires_modifier =
-            all_language_settings(file, cx).edit_predictions_mode() == EditPredictionsMode::Auto;
+        let preview_requires_modifier = all_language_settings(file, cx).edit_predictions_mode()
+            == EditPredictionsMode::WhenHoldingModifier;
 
         EditPredictionSettings::Enabled {
             show_in_menu,
